d9f58bea5421096383827943ba48752ae047995b
Samukelo khathi <145541253+Samukelokhathi@users.noreply.github.com>
2026-02-11 15:50:01 +0200

    updated contact image

--- src/windows/Contact.jsx
+++ src/windows/Contact.jsx
@@ -11,15 +11,17 @@ const Contact = () => {
       </div>
 
       <div className="p-5 space-y-5 ">
-        <img src="images/1.sam.jpg" alt="" className="w-40 rounded-full" />
-
+        <img
+          src="images/1.sam.jpg"
+          alt="Sumkelows image"
+          className="w-40 rounded-full"
+        />
         <h3>Let's Connect</h3>
         <p>
           +27 69 159 4100 <br />
           sbkhathi005@gmail.com
         </p>
         <p>Got an idea? A bug to terminate? Or just wanna talk tech?</p>
-
         <ul>
           {socials.map(({ id, bg, link, icon, text }) => (
             <li key={id} style={{ backgroundColor: bg }}>
